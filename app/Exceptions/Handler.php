f4b6d299d50deecd47fd7ecf699102094e15224e
saw <saw>
2023-04-11 14:45:11 +0800

    Create 404 Function

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,4 +45,15 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    // Create the 404 Page Function
+    public function render($request, Throwable $exception)
+    {
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() == 404) {
+                return response()->view('errors.404', [], 404);
+            } 
+        }
+
+        return parent::render($request, $exception);
+    }
 }
